3685f3fcef802ae964cad50d2c86bf6e0b43799f
JooYoo <jooyoo@outlook.com>
2021-07-19 09:58:14 +0200

    dev: add inputs DOM

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,6 +4,28 @@ const Form = () => {
   return (
     <form>
       <h4>Form</h4>
+
+      <div>
+        <label>Name</label>
+        <input type="text" />
+      </div>
+
+      <div>
+        <label>Comment</label>
+        <textarea></textarea>
+      </div>
+
+      <div>
+        <select value="Vue">
+          <option value="React">React</option>
+          <option value="Vue">Vue</option>
+          <option value="Angular">Angular</option>
+        </select>
+      </div>
+
+      <br />
+
+      <button type="submit">Submit</button>
     </form>
   );
 };
